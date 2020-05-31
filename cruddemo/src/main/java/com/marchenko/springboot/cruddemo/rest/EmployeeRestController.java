83b9333107a8bf95383c184ac604903f6402817c
Vitalii Marchenko <vitalymarchenko@Vitalys-MacBook-Pro.local>
2020-05-31 16:14:13 +0300

    Setup project, add basic cotrollers

---
+++ cruddemo/src/main/java/com/marchenko/springboot/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,42 @@
+package com.marchenko.springboot.cruddemo.rest;
+
+import com.marchenko.springboot.cruddemo.entity.Employee;
+import com.marchenko.springboot.cruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeRestController(EmployeeService theEmployeeService) {
+        this.employeeService = theEmployeeService;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getEmployees() {
+        return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployeeById(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        employee.setId(0);
+
+        employeeService.save(employee);
+        return employee;
+    }
+}
