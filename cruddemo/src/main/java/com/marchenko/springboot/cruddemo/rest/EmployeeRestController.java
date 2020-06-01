8e6d82d4c66b1f236fb71393576de37490ca1f82
Vitalii Marchenko <vitalymarchenko@Vitalys-MacBook-Pro.local>
2020-06-01 09:26:59 +0300

    Finish with controllers using Hibernate API

--- cruddemo/src/main/java/com/marchenko/springboot/cruddemo/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/com/marchenko/springboot/cruddemo/rest/EmployeeRestController.java
@@ -39,4 +39,21 @@ public class EmployeeRestController {
         employeeService.save(employee);
         return employee;
     }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        employeeService.save(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        employeeService.deleteById(employeeId);
+        return "Deleted employee with id: " + employeeId;
+    }
 }
